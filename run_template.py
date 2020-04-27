f31afb8d342763dbc10fe36a31f2e29dff30923a
Stephen Stokes <rv008@RV008-MacBook-Pro.local>
2020-04-27 10:20:51 -0400

    202004271020

--- run_template.py
+++ run_template.py
@@ -3,16 +3,27 @@
 
 __author__ = 'Stephen Stokes: sstokes[at]relationshipvelocity.com'
 
+import logging
 import sys
+
 import sf_tdm as tdm
+import tools.helpers as h
+
+# Logging setup
+h.setup_logging()
+
+# Logging statements for each module:
+log = logging.getLogger(__name__)
+log.debug(f'{__name__} logging is configured.')
 
 # Get script argument
 try:
     config = sys.argv[1]
 except Exception as argument_error:
-    print(f'No input arguments.\nError: {argument_error}')
+    # print(f'No input arguments.\nError: {argument_error}')
+    log.exception(f'No input arguments.\nError: {argument_error}')
     raise
 
 # Run template
 results = tdm.run_template(tdm_config=config)
-print(f'{results}\n')
+log.info(f'{results}\n')
