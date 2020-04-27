3934ce0283e175cb7e2bbc01f52599b9318d6ffe
Stephen Stokes <rv008@RV008-MacBook-Pro.local>
2020-04-27 11:50:19 -0400

    202004271150

--- run_template.py
+++ run_template.py
@@ -9,9 +9,6 @@ import sys
 import sf_tdm as tdm
 import tools.helpers as h
 
-# Logging setup
-h.setup_logging()
-
 # Logging statements for each module:
 log = logging.getLogger(__name__)
 log.debug(f'{__name__} logging is configured.')
